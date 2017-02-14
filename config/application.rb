0e89d927b42b8b5c17eab46f6804633a03299da7
Yiliang Tang <ydatylmonv@gmail.com>
2017-02-14 21:23:54 +0800

    feat: Setup sidekiq

--- config/application.rb
+++ config/application.rb
@@ -12,5 +12,8 @@ module Trust
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.generators.javascript_engine :js
+
+
+    config.active_job.queue_adapter = :sidekiq
   end
 end
