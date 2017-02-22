28ddb6560c6ef99a23eeb04856164ca24bc9e61f
Yiliang Tang <ydatylmonv@gmail.com>
2017-02-22 10:02:14 +0800

    feat: Add index messages.created_at

--- app/jobs/message_broadcast_job.rb
+++ app/jobs/message_broadcast_job.rb
@@ -6,12 +6,4 @@ class MessageBroadcastJob < ApplicationJob
       message: { user_id: message.user_id, username: message.user.username, body: message.body,
                  created_at: message.friendly_create_at, message_id: message.id }
   end
-  #
-  # private
-  #   def render_message(message)
-  #     ApplicationController.renderer.render(
-  #       partial: 'messages/message',
-  #       locals: { message: message, current_user: message.user}
-  #     )
-  #   end
 end
