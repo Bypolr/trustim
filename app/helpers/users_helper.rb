16daefaea59889ffb3f26782dcd046e35ff70dac
towry <tovvry@gmail.com>
2017-04-08 00:09:12 +0800

    Style the main page.
    * Remove invalid tests.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,11 +1,11 @@
 module UsersHelper
 	def gravatar_for(user, options = {})
-    options_default = { size: 80 }
+    options_default = { size: 80, class: "gravatar" }
     options.reverse_merge!(options_default)
 
 		gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
 		gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?size=#{options[:size]}"
-		image_tag(gravatar_url, alt: user.username, class: "gravatar")
+		image_tag(gravatar_url, alt: user.username, class: options[:class])
 	end
 
   def show_unread_count
